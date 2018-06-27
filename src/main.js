1db7ad667a67b9a01b773eedbb1f00e949d5f131
Parminder Klair <perminder.klair@gmail.com>
2018-06-27 17:17:21 +0530

    fetch jobs from indeed and reed

--- src/main.js
+++ src/main.js
@@ -1,12 +1,163 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
+import Turndown from 'turndown';
+import scrapeIt from 'scrape-it';
+import striptags from 'striptags';
+import Url from 'url';
 
-export default function howLongUntilLunch(hours, minutes) {
-	// lunch is at 12.30
-	if (hours === undefined) hours = 12;
-	if (minutes === undefined) minutes = 30;
+export async function fetchIndeedJobs(url) {
+  try {
+    const result = await new Promise(((resolve, reject) => {
+      scrapeIt(url, {
+        jobs: {
+          listItem: '.row.result',
+          data: {
+            title: {
+              selector: '.jobtitle > a',
+              how: 'html',
+              convert: x => striptags(x),
+            },
+            url: {
+              selector: '.jobtitle > a',
+              attr: 'href',
+              convert: x => `https://www.indeed.co.uk${x}`,
+            },
+            location: '.location',
+            company: '.company',
+          },
+        },
+      })
+        .then(({ data, response }) => {
+          if (response.statusCode !== 200) reject(new Error('data fetch failed'));
+          resolve(data);
+        });
+    }));
 
-	var millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-	return ms(millisecondsUntilLunchTime, { long: true });
+    // console.log('result', result);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+
+export async function fetchIndeedJob(url) {
+  try {
+    const result = await new Promise(((resolve, reject) => {
+      scrapeIt(url, {
+        jobTitle: '.jobtitle font',
+        locationName: '#job-content span.location',
+        employerName: '#job-content span.company',
+        jobDescription: {
+          selector: '#job_summary div',
+          how: 'html',
+          convert: (x) => {
+            const turndown = new Turndown();
+            return turndown.turndown(x);
+          },
+        },
+      })
+        .then(({ data, response }) => {
+          if (response.statusCode !== 200) reject(new Error('data fetch failed'));
+          resolve(data);
+        });
+    }));
+    // console.log('result', result);
+
+    const urlParts = Url.parse(url, true);
+    const jobId = urlParts.query.jk;
+    const data = {
+      ...result,
+      jobUrl: url,
+      jobId,
+    };
+    // console.log('data', data);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
+export async function fetchReedJob(url) {
+  try {
+    const result = await new Promise(((resolve, reject) => {
+      scrapeIt(url, {
+        jobTitle: {
+          selector: 'meta[itemprop="title"]',
+          attr: 'content',
+        },
+        companyLogo: {
+          selector: '.logo-wrap meta[itemprop="image"]',
+          attr: 'content',
+          convert: x => (x ? x.slice(0, -17) : null),
+        },
+        datePosted: {
+          selector: 'meta[itemprop="datePosted"]',
+          attr: 'content',
+        },
+        expirationDate: {
+          selector: 'meta[itemprop="validThrough"]',
+          attr: 'content',
+        },
+        employerName: {
+          selector: '.posted span[itemprop="name"]',
+          how: 'text',
+        },
+        industry: {
+          selector: 'meta[itemprop="industry"]',
+          attr: 'content',
+        },
+        employmentType: {
+          selector: 'span[itemprop="employmentType"]',
+          attr: 'content',
+        },
+        workHours: {
+          selector: 'meta[itemprop="workHours"]',
+          attr: 'content',
+        },
+        locationName: {
+          selector: 'meta[itemprop="addressRegion"]',
+          attr: 'content',
+        },
+        salary: {
+          selector: 'span[itemprop="baseSalary"] meta[itemprop="value"]',
+          attr: 'content',
+        },
+        salaryType: {
+          selector: 'span[itemprop="baseSalary"] meta[itemprop="unitText"]',
+          attr: 'content',
+        },
+        jobDescription: {
+          selector: '.description span[itemprop="description"]',
+          how: 'html',
+          convert: (x) => {
+            const turndown = new Turndown();
+            return turndown.turndown(x);
+          },
+        },
+        skills: {
+          selector: 'ul[itemprop="skills"] li[itemprop="experienceRequirements"]',
+          how: 'text',
+        },
+        jobUrl: {
+          selector: 'meta[itemprop="url"]',
+          attr: 'content',
+        },
+        jobId: {
+          selector: 'input#JobId',
+          how: 'val',
+        },
+      })
+        .then(({ data, response }) => {
+          if (response.statusCode !== 200) reject(new Error('data fetch failed'));
+          resolve(data);
+        });
+    }));
+
+    // console.log('result', result);
+    return result;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,53 +1,59 @@
 const assert = require('assert');
-const howLongTillLunch = require('..');
+const { fetchIndeedJobs, fetchIndeedJob, fetchReedJob } = require('..');
 
-function MockDate () {
-	this.date = 0;
-	this.hours = 0;
-	this.minutes = 0;
-	this.seconds = 0;
-	this.milliseconds = 0;
-};
 
-Object.assign(MockDate.prototype, {
-	getDate () { return this.date; },
-	setDate (date) { this.date = date; },
-	setHours (h) { this.hours = h; },
-	setMinutes (m) { this.minutes = m; },
-	setSeconds (s) { this.seconds = s; },
-	setMilliseconds (ms) { this.milliseconds = ms; },
-	valueOf () {
-		return (
-			this.milliseconds +
-			this.seconds * 1e3 +
-			this.minutes * 1e3 * 60 +
-			this.hours * 1e3 * 60 * 60 +
-			this.date * 1e3 * 60 * 60 * 24
-		);
-	}
+describe('Job fetcher', () => {
+  describe('fetch Reed job data', () => {
+    it('should return object with job data', async () => {
+      const url = 'https://www.reed.co.uk/jobs/front-end-developer-london-350-400-per-day/35374312';
+      const expected = {
+        jobId: '35374312',
+        jobUrl: 'https://www.reed.co.uk/company-profile/Pearson-Frank-49241?jobId=35374312',
+        companyLogo: 'https://resources.reed.co.uk/profileimages/logos/thumbs/Logo_40437.png',
+        jobTitle: 'Front End Developer',
+        datePosted: '2018-06-12',
+        expirationDate: '2018-07-24T23:55:00.0000000',
+        employerName: 'Pearson Frank',
+        industry: 'IT & Telecoms',
+        employmentType: 'FULL_TIME, CONTRACTOR',
+        workHours: 'full-time',
+        locationName: 'London',
+        salary: '350.0000',
+        salaryType: 'DAY',
+        jobDescription: 'ReactJS Developer - London - £350 - £400 per day - 6 month contract (minimum)  \nCity of London  \nGreenfield Banking Project  \n  \nPearson Frank are currently working in partnership with a Global Consultancy who are searching for Front End Developers with expertise and commercial experience with ReactJS for a 6 month contract with possible extensions and an immediate start. Contract rates on offer are between £350 - £400 per day.  \n  \nSkills & Experience:  \n  \nFront End Developer:  \n\\- ReactJS is a mandatory skill and all other Web Technologies are a bonus  \n  \nIf you meet the above requirements and believe you would be a suitable candidate, apply now to avoid missing out on this fantastic opportunity as the role will not be on the market long.  \n  \nPearson Frank International is the UK Market Leader in Open Source and Digital recruitment. All our years of experience in the IT industry have allowed us to branch out and specialise in specific markets and we now also have dedicated teams working with in Open Source Development Technologies, Mobile Application Development and Web Application Development.  \n  \nApply now by sending your CV to -  \n  \nOr call Ben Townsend - (+44)  \n  \nFront End Developer HTML5 CSS3 JQuery Javascript DOJO NodeJS AngularJS ReactJSFront End Developer HTML5 CSS3 JQuery Javascript DOJO NodeJS AngularJS ReactJSJava, Front End, Developer, J2EE, Spring, Web Service, SQL, SQL Queries , XML, Spring MVC, Hibernate, Web Application, HTML, Javascript, TDD, Junit, Mockito, Jmock, London, Central London, Essex, Hertfordshire, Buckinghamshire, Berkshire, Surrey, Sussex, Kent REACT REACT REACT',
+        skills: 'Front End Developer HTML5 CSS3 JQuery Javascript DOJO NodeJS AngularJS ReactJS React ReactJS',
+      };
+
+      const response = await fetchReedJob(url);
+      assert.deepEqual(response, expected);
+      return true;
+    });
+  });
+
+  describe('fetching Indeed jobs list data', () => {
+    it('should return array of 10 objects with all jobs data', async () => {
+      const url = 'https://www.indeed.co.uk/jobs?q=react&l=london&start=0';
+      const expected = 10;
+      const response = await fetchIndeedJobs(url);
+      assert.deepEqual(response.jobs.length, expected);
+      return true;
+    });
+  });
+
+  describe('fetch Indeed job data', () => {
+    it('should return object with job data', async () => {
+      const url = 'https://www.indeed.co.uk/viewjob?jk=76ba8672a95f102e&from=serp&vjs=3';
+      const expected = {
+        jobTitle: 'Frontend Engineer (React)',
+        locationName: 'London',
+        employerName: 'PlentificExpend LtdSport WhispersClient ServerAmpers & ConsultingiGenius',
+        jobDescription: '**The company**\n\nPlentific is one of UK’s fastest growing technology start-ups. We provide homeowners, landlords and property managers a transactional marketplace to find and hire repairs, maintenance and home improvement professionals.\n\n**The team**\n\nThe engineering team sits at the centre of everything we do at Plentific and is constantly tackling challenging problems, such as online payments, quoting, invoicing, booking, search / scoring algorithms, ETL, data pipelines, in-app messaging, real-time notifications and fraud prevention. Our frontend engineers mostly work with JavaScript (ES6, React, TypeScript, Alt.js, Node.js, Express) on an increasingly more service-oriented architecture. The rest of the tech stack include Django REST Framework, PostgreSQL, PostGIS, AWS, Amazon Redshift, Kubernetes, Docker, Redis, Celery, Pandas, Numpy, Scrapy, Git with Zenhub, Jenkins, Elasticsearch, Logstash and lots of raw SQL for analytics. We have a very large but clean code base as we put significant emphasis on design patterns, code readability, testability, maintainability and extendability.\n\n**The job**\n\nWe’re looking for an experienced frontend engineer to join the engineering team. You’ll be working alongside a highly technical and motivated team and report to the technical co-founder. There is a specific reason why we use the term \\`engineer\\` over \\`developer\\`. You would be expected to apply fundamental engineering and mathematical skills to solve problems and overcome challenges, not just develop code. For people with the right mindset, this frame of mind provides an intellectually stimulating environment.\n\n**Requirements**\n\n*   Expert level knowledge of JavaScript (ES6, TypeScript)\n*   Strong computer science fundamentals and JavaScript design patterns\n*   1+ year commercial React experience using one of the Flux frameworks\n*   Strong understanding of UI/UX best practices and considerations\n*   Passion for writing clean, modular, well-commented, readable and reusable code\n*   Ability to think out of the box with a can-do attitude to get things done efficiently\n*   Excellent communication skills with ability to articulate technical concepts in plain English\n*   Top academics (BSc, MSc) in computer science, engineering, physics, mathematics or another field with strong programming curriculum\n\n**Benefits**\n\n*   Competitive compensation package\n*   A new 15’’ MacBook pro and supporting hardware of your choice\n*   Learning and development fund\n*   23 day holidays\n*   A stylish office in the heart of Old Street, London\n*   Stocked kitchen and breakfast bar with great coffee',
+        jobUrl: 'https://www.indeed.co.uk/viewjob?jk=76ba8672a95f102e&from=serp&vjs=3',
+        jobId: '76ba8672a95f102e',
+      };
+      const response = await fetchIndeedJob(url);
+      assert.deepEqual(response, expected);
+      return true;
+    });
+  });
 });
-
-const now = new MockDate();
-MockDate.now = () => now.valueOf();
-
-global.Date = MockDate;
-
-function test(hours, minutes, seconds, expected) {
-	now.setHours(hours);
-	now.setMinutes(minutes);
-	now.setSeconds(seconds);
-
-	assert.equal(howLongTillLunch(...lunchtime), expected);
-	console.log(`\u001B[32m✓\u001B[39m ${expected}`);
-}
-
-let lunchtime = [ 12, 30 ];
-test(11, 30, 0, '1 hour');
-test(10, 30, 0, '2 hours');
-test(12, 25, 0, '5 minutes');
-test(12, 29, 15, '45 seconds');
-test(13, 30, 0, '23 hours');
-
-// some of us like an early lunch
-lunchtime = [ 11, 0 ];
-test(10, 30, 0, '30 minutes');
